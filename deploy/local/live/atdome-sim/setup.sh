53ef945c518534d5c8e60e33999b2d0bd096f50c
José  Galaz <jdgalazm@gmail.com>
2019-06-17 18:51:53 -0400

    build atdome-sim locally in deploy/local/live

---
+++ deploy/local/live/atdome-sim/setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Source this file when starting the container to set it up
+
+echo "#"
+echo "# Loading LSST Stack"
+. /opt/lsst/software/stack/loadLSST.bash
+setup lsst_distrib
+echo "#"
+echo "# Loading sal environment"
+. repos/ts_sal/setup.env
+echo "#"
+echo "# Setting up sal, salobj and scriptqueue"
+
+setup ts_xml -t current
+setup ts_sal -t current
+setup ts_salobj -t current
+setup ts_scriptqueue -t current
+setup ts_config_attcs -t current
+
+setup ts_ATDome -t $USER 
+
+cd /home/saluser/repos/ts_ATDome/bin/
+
+echo "# Starting ATDome Simulator CSC"
+python ./run_atdome.py -s
