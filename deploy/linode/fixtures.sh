e0cc1c92c8fc9acf08f5cb240d39feeba816afe7
José  Galaz <jdgalazm@gmail.com>
2020-08-18 21:04:19 -0400

    cleanup data and thumbnails and load them on local/live

--- deploy/linode/fixtures.sh
+++ deploy/linode/fixtures.sh
@@ -10,6 +10,8 @@ scp -r love@dev.love.inria.cl:~/media fixtures/media
 # copy everything into the live mode version
 
 echo "Replacing local data"
+sudo rm -rf db_data
+sudo rm -f media/thumbnails/*
 manager=../../../LOVE-manager
 cp fixtures/test.json  $manager/manager/ui_framework/fixtures/initial_data.json
 rm $manager/manager/media/thumbnails/*.png
@@ -19,13 +21,9 @@ echo "Available thmbnails before copying: "
 ls $manager/manager/media/thumbnails
 
 cp fixtures/media/thumbnails/*.png  $manager/manager/media/thumbnails/.
+rm -rf ../local/live/media
+cp -r fixtures/media ../local/live/media
 echo
 echo "Available thmbnails after copying: "
 ls $manager/manager/media/thumbnails
-
-
-echo
-echo "Removing db_data"
-rm -rf db_data
-echo "Done"
 echo
