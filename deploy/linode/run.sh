df3eef402de295697cd45be1897870e6a9523ba5
spereirag <sebastian.pereira@inria.cl>
2020-12-22 16:23:20 -0300

    Update deploy script for linode env

--- deploy/linode/run.sh
+++ deploy/linode/run.sh
@@ -3,7 +3,7 @@ docker network inspect testnet >/dev/null 2>&1 || docker network create testnet
 docker network inspect testnet >/dev/null 2>&1 || docker network create testnet
 
 # update ts repos needed
-rm -rf ts_externalscripts
+rm -rf ts_standardscripts
 rm -rf ts_externalscripts
 git clone --depth 1 https://github.com/lsst-ts/ts_standardscripts.git
 git clone --depth 1 https://github.com/lsst-ts/ts_externalscripts.git
